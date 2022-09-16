3c1c6f021ce5033782e684bfac93dad4d2bd2920
Ludo <ludoattuoni@gmail.com>
2022-09-16 20:47:08 +0100

    refactor- tried to adjust likelihood

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-
-from telnetlib import SE
-
-
 PARENTING = 0
 SOCIAl = 1
 FINANCIAL = 2
@@ -10,11 +6,15 @@ COUNSELING = 3
 NUM_CATEGORIES = 4
 
 class Question:
+    #question string
     q_string = ""
+    #weights for yes answer
     weights_yes = []
+    #weights for no answer
     weights_no = []
     def __init__(self,q_string, weights_y,weights_n = 0):
         if weights_n == 0:
+            #creates a inverse of the weights list
             weights_n = [1-x for x in weights_y]
         self.q_string = q_string
         self.weights_yes= weights_y
@@ -38,14 +38,20 @@ class BayesianUpdater:
         self.questions.append(Question("Do you have dark thoughts?", [0.5,0.6,0.5,0.9]))
 
         self.services.append(Service('Parent Group', [0.8,0.8,0,0]))
-        self.services.append(Service('Samaritans', [0,0.5,0,0.8]))
+        self.services.append(Service('Samaritans', [0,0,0,0.8]))
         self.services.append(Service('Financial Advise', [0,0,0.8,0]))
 
 
     def AskQuestion(self):
-        q = self.questions[self.question_index%len(self.questions)]
-        self.question_index+=1
-        return q.weights_yes if 'y' in input(q.q_string) else q.weights_no
+        q = self.SelectQuestion()
+        if 'y' in input(q.q_string):
+            return q.weights_yes  
+        return q.weights_no
+
+    def SelectQuestion(self):
+        index = 0#self.question_index%len(self.questions)
+        self.question_index += 1
+        return self.questions[index]
 
     def Update(self):
         data = self.AskQuestion()
@@ -54,7 +60,7 @@ class BayesianUpdater:
             prior = self.states[x]
             d = data[x]
             #needs refining
-            likelihood = ((d - prior)/2)+0.5
+            likelihood = ((d - prior) /2) + 0.5
             
             
             
@@ -62,13 +68,20 @@ class BayesianUpdater:
             #print(prior,d,likelihood)
             posterior = (prior * likelihood)/normalisation
             self.states[x] = posterior
-
+    def CalculateDistanceOfWeights(self,service_weights):
+        squared_sum = 0
+        for x in range(NUM_CATEGORIES):
+            euclidean_dist = service_weights[x] - self.states[x]
+            squared_dist = pow(euclidean_dist,2)
+            squared_sum += squared_dist
+        
+        return pow(squared_sum, 0.5)
     def BestSuggestion(self):
-        threshold = 0.85
+        threshold =0.3 
         suggestions = []
         for i in range(len(self.services)):
             service = self.services[i]
-            dist = pow(sum([ pow(service.weights[x] - self.states[x],2) for x in range(NUM_CATEGORIES) ]),0.5)
+            dist = self.CalculateDistanceOfWeights(service.weights)
             print(dist)
             if dist < threshold:
                 suggestions.append(service)
@@ -78,7 +91,7 @@ class BayesianUpdater:
         self.Update()
         for s in self.BestSuggestion():
             print(s.service_name)
-        #print(self.states)
+        print(self.states)
 
     
 bu = BayesianUpdater()
