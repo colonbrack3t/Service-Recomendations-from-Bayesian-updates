c0cc55dccf2bef94f5b6ed90b77d1e4aba5a5697
Ludo <ludoattuoni@gmail.com>
2022-09-18 18:57:25 +0100

    Documented

--- main.py
+++ main.py
@@ -1,55 +1,109 @@
+'''
+Library imports
+'''
 import sys
 from math import pi, exp
 from scipy.stats import norm
+
+'''
+Constants
+'''
 
 PARENTING = 0
 SOCIAL = 1
 FINANCIAL = 2
 COUNSELING = 3
-#copy & paste to create legible dicts
+#copy & paste to create legible dicts or use category_dict.copy() to create an instance of this dict
 category_dict = {
     PARENTING : 0.5,
     SOCIAL : 0.5,
     FINANCIAL : 0.5,
     COUNSELING : 0.5
 }
+
 NUM_CATEGORIES = 4
+
+'''
+Use these preset standard deviations for creating distributions
+'''
 
 CERTAIN_PROBABILTY_SD = 0.5
 MEDIUM_PROBABILITY_SD = 1
 UNCERTAIN_PROBABILITY_SD = 3
 
+
+
 class QuestionCategoryProbability:
+    """This class represents a Normal Distribution probability for a given question's category
+    """
     mean = 0
     standard_deviation = 0
-    def __init__(self, m, sd):
+    def __init__(self, m:float, sd:float):
+        """Constructor
+
+        Args:
+            m (float): mean
+            sd (float): standard deviation
+        """
+        
         self.mean = m
         self.standard_deviation = sd
-    def likelihood(self,x):
+    def likelihood(self,x:float) ->float:
+        """Likelihood estimation. Uses CDF
+        Args:
+            x (float): given state
         
+        Returns:
+            float: Likelihood of observed question, given state x
+        """
         return norm(x,self.standard_deviation).cdf(self.mean)
     def inverse(self):
+        """Inverts this QCP mean
+        """
         self.mean = 1- self.mean
     def __str__(self):
         return f"mean = {self.mean}, sd = {self.standard_deviation}"
-all_qs = []
+
 class Question:
     #question string
     m_question_string = ""
     #weights for yes answer
-    weights_yes = 0
+    weights_yes : dict = {}
     #weights for no answer
-    weights_no = 0
+    weights_no : dict = {}
     def Set_Collection_as_weights(self, weights_y, weights_n):
+        """Generalised function for turning array or dict of weights input to constructor into the weights array for this class
+
+        Args:
+            weights_y : Weights for 'yes' response
+            weights_n : Weights for 'no' response
+        """
+
         for i in range(NUM_CATEGORIES):
-                #unknown desired probability, as only mean value presented, so assume a medium SD
                 vy = weights_y[i]
                 vn = weights_n[i]
+
+                #unknown desired probability, as only mean value presented, so assume a medium SD
                 QCP_y = QuestionCategoryProbability(vy,UNCERTAIN_PROBABILITY_SD)
                 QCP_n = QuestionCategoryProbability(vn,UNCERTAIN_PROBABILITY_SD)
                 self.weights_yes[i] = QCP_y
                 self.weights_no[i] = QCP_n 
-    def __init__(self,q_string,**kwargs):
+
+    def __init__(self,q_string:str,**kwargs):
+        """
+        Args:
+            q_string (str): Question text
+            **flat_array_y : array of floats representing average for each category, for 'yes' response
+            **flat_array_n : (Optional, will be generated from inverse of y if omitted)
+            
+            **flat_dict_y : dict of floats representing average for each category, for 'yes' response
+            **flat_dict_y : (Optional, will be generated from inverse of y if omitted)
+            
+            **distributions_yes : dict of QuestionCategoryProbability for 'yes' response
+            **distributions_yes : (Optional, will be generated from inverse of yes if omitted)
+
+        """
+        #create copys so that they do not point to the same dict obj in memory
         self.weights_yes = category_dict.copy()
         self.weights_no = category_dict.copy()
         self.m_question_string = q_string
@@ -89,7 +143,12 @@ class Question:
 class Service:
     service_name = ""
     weights = []
-    def __init__(self, service_name, weights):
+    def __init__(self, service_name:str, weights:list):
+        """
+        Args:
+            service_name (str): Name of service
+            weights (list): float list of weights for each category for service
+        """
         self.service_name = service_name
         self.weights = weights
         
@@ -100,6 +159,10 @@ class BayesianUpdater:
     question_index = 0
     log = False
     def __init__(self,log = False):
+        """
+        Args:
+            log (bool, optional): If true, additional print statements showing internal calculations. Defaults to False.
+        """
         self.log = log
         self.questions.append(Question('Do you have kids?', flat_array_y = [0.8,0.5,0.6,0.5]))
         self.questions.append(Question("Do you spend time with friends?", flat_array_y = [0.5,0.1,0.4,0.4]))
@@ -125,25 +188,17 @@ class BayesianUpdater:
 
 
     def AskQuestion(self):
-        q = self.SelectQuestion()
-    
+        q : Question = self.SelectQuestion()
         if 'y' in input(q.m_question_string):
             return q.weights_yes  
         return q.weights_no
 
-    def SelectQuestion(self):
+    def SelectQuestion(self) -> Question:
         index = self.question_index%len(self.questions)
         self.question_index += 1
         return self.questions[index]
-    def CalculateLikelihood(self, d, prior):
-        centralise_data = d - 0.5
-        #naive likelihood calculation
-        likelihood = prior + centralise_data
-        likelihood = max(likelihood, 0.000001)
-        likelihood = min(likelihood, 0.999999)
 
-        return d#likelihood
-    def CalculateNormalisation(self, data, d):
+    def CalculateNormalisation(self, data:list, d:QuestionCategoryProbability):
         return 0.5
         
     def Update(self):
@@ -151,19 +206,17 @@ class BayesianUpdater:
 
         for x in range(NUM_CATEGORIES):
             prior = self.states[x]
-            d = data[x]
-            #needs refining
+            d : QuestionCategoryProbability = data[x]
             likelihood = d.likelihood(prior)
-            
-            
-            
-            normalisation = 0.5# self.CalculateNormalisation(data,d)
+            normalisation = self.CalculateNormalisation(data,d)
             if self.log:
                 print(x, 'Prior:',prior,', z mean:',d.mean,',\nz standard deviation:',d.standard_deviation,', Likelihood:',likelihood)
+            
+            #Bayesian Update:
             posterior = (prior * likelihood)/normalisation
            
             self.states[x] = posterior
-    def CalculateDistanceOfWeights(self,service_weights):
+    def CalculateDistanceOfWeights(self,service_weights:dict)->float:
         squared_sum = 0
         for x in range(NUM_CATEGORIES):
             euclidean_dist = service_weights[x] - self.states[x]
@@ -185,6 +238,8 @@ class BayesianUpdater:
         return suggestions
     
     def Cycle(self):
+        """Asks question, updates weights, then offers suggestions
+        """
         self.Update()
         for s,d in self.BestSuggestion():
             print(s.service_name, f'{round((1 - d) * 100) }%')
